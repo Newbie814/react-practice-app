57140791da962ed924d8340dbccfc8bdc0413c12
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-05-23 23:43:53 -0400

    finished card mapping and rendering

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
 import Card from './Card';
+import Cardlist from './Cardlist';
 import { robots } from './robots';
 
 import reportWebVitals from './reportWebVitals';
@@ -11,10 +12,10 @@ import 'tachyons';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
-    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
-    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
+    {
+      /* <App /> */
+      <Cardlist robots={robots} />
+    }
   </React.StrictMode>
 );
 

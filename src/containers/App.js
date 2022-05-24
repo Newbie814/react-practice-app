e6aa8ab9223d7a145adbcaf67c477876823ff035
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-05-24 19:34:06 -0400

    refactored folder structure

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import Cardlist from './Cardlist';
+import Cardlist from '../components/Cardlist';
 
-import SearchBox from './SearchBox';
-import Scroll from './Scroll';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
 import './App.css';
 
 class App extends Component {

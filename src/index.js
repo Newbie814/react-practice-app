6f3facbd5d54df5ed955a595b1c5134c3e948b6a
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-05-24 18:08:48 -0400

    search function and rendering

--- src/index.js
+++ src/index.js
@@ -1,23 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
-import Card from './Card';
-import Cardlist from './Cardlist';
-import { robots } from './robots';
+
+import App from './App';
 
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    {
-      /* <App /> */
-      <Cardlist robots={robots} />
-    }
-  </React.StrictMode>
-);
+root.render(<React.StrictMode>{<App />}</React.StrictMode>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

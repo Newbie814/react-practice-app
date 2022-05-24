628304b92092ad3b7c959256653c664168e676e4
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-05-24 19:22:36 -0400

    added styling and scroll component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
 import Cardlist from './Cardlist';
-import { robots } from './robots';
+
 import SearchBox from './SearchBox';
+import Scroll from './Scroll';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: '',
     };
+  }
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => {
+        return response.json();
+      })
+      .then((users) => {
+        this.setState({ robots: users });
+      });
   }
 
   onSearchChange = (event) => {
@@ -23,14 +33,19 @@ class App extends Component {
         .toLowerCase()
         .includes(this.state.searchfield.toLowerCase());
     });
-
-    return (
-      <div className='tc'>
-        <h1 className='f1'>RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <Cardlist robots={filteredRobots} />
-      </div>
-    );
+    if (this.state.robots.length === 0) {
+      return <h1>Loading</h1>;
+    } else {
+      return (
+        <div className='tc'>
+          <h1 className='f1'>RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <Scroll>
+            <Cardlist robots={filteredRobots} />
+          </Scroll>
+        </div>
+      );
+    }
   }
 }
 
